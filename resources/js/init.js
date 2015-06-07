837704e8fa1fbc6bbdeedb2cd647d5c9b719011e
hzitoun <hzitoun@enseirb-matmeca.fr>
2015-06-07 19:54:21 +0200

    adding history

--- resources/js/init.js
+++ resources/js/init.js
@@ -9,6 +9,8 @@
 	var userBase = new Firebase("https://resplendent-fire-5470.firebaseio.com/");
 	var myUserRef = userBase.push();
 	var connectedRef = new Firebase("https://resplendent-fire-5470.firebaseio.com//.info/connected");
+	var histoBase = new Firebase("https://8k98sj870409hns897h4.firebaseio.com/");
+	var histoRef = histoBase.push();
 	$( "#join" ).click(function() {
 		currentUser = $('#customUS').val();
 		if(currentUser != ''){
@@ -55,10 +57,13 @@
 		$("#connectedDiv").children("#" + getMessageId(element))
 		.html('<div>' + user.username + " is " + user.status + '<hr class="hr-clas-low" />')
 	});
-
 	function setUserStatus(status) {
 		currentStatus = status;
 		if(currentUser != ''){
 			myUserRef.set({ username: currentUser, status: status });
+ 			var now = new Date();
+    		var dateToPersist = now.toLocaleFormat('%d-%b-%Y %H:%M');
+			histoRef.set({ username: currentUser, status: status, date : dateToPersist  });
+			
 		}
 	}
