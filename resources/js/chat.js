c39abbf8d3d2f5c1c68d6086ad455002bc3a1347
Hamed ZITOUN <zitoun.hamed@gmail.com>
2016-09-20 14:22:03 +0200

    Update chat.js

--- resources/js/chat.js
+++ resources/js/chat.js
@@ -6,14 +6,14 @@
 */
 var chatBase = new Firebase('https://fbdchat.firebaseio.com/');
 $('#messageInput').keypress(function (e) {
-  if (e.keyCode == 13) {
+  if (e.keyCode === 13) {
     fnSendMsg();
   }
 });
 var msgCount = 0;
 chatBase.on('child_added', function(snapshot) {
   msgCount++;
-  if(msgCount == 1){
+  if(msgCount === 1){
     $('#messagesDiv').html('');
   }
   var message = snapshot.val();
@@ -22,15 +22,15 @@ chatBase.on('child_added', function(snapshot) {
 var direction = '';
 function addMsg(username, msg, date) {
   direction = 'left'; 
-  if(msgCount % 2 == 0)
-   direction = 'right'; 
+  if(msgCount % 2 === 0) {
+    direction = 'right'; 
+  }
  $('<div/>').append('<div title="'+date+'" class="chat-box-'+direction+'">' + msg + '</div>').append('<div class="chat-box-name-'+direction+'"><img src="resources/img/user.png" alt="bootstrap Chat box user image" class="img-circle" /> -'+ username + ' </div><hr class="hr-clas" />').appendTo($('#messagesDiv'));
  $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
 };
 function fnSendMsg(){
   var  msg = $('#messageInput').val();
-  if(msg != '')
-  {
+  if (msg !== '') {
     var now = new Date();
     var dateToPersist = now.toLocaleFormat('%d-%b-%Y %H:%M');
     chatBase.push({ date : dateToPersist, msg: msg, username: currentUser});
@@ -38,7 +38,7 @@ function fnSendMsg(){
   }
 }
 
-if(msgCount === 0){
+if (msgCount === 0) {
   $('#messagesDiv').html('<b>Don\'t be Shy and Say Hey !</b>');
 }
 
